49808c9560470b791710947f0725d4c6bd2ace91
paru93 <kulkarni.parimal11@gmail.com>
2017-03-14 12:47:26 -0700

    modified script

--- input_file_gen.py
+++ input_file_gen.py
@@ -1,16 +1,30 @@
 from random import randint
 import os
-
-def generator(n):
-    for i in range(0,n):
-        if(i==n-1):
+address=0
+proc_id=0
+rw=0
+data=0
+def generator(no_memory_lines,no_processors,no_transactions):
+    for i in range(0,no_transactions):
+        if(i==no_transactions-1):
             a=''
         else:
             a='\n'
-        fp.write(str((randint(1,15)%2 + 1))+" "+hex(randint(1,99999999))+" "+str((randint(1,15)%2))+" "+hex(randint(1,99999999))+a)
+            proc_id=str(randint(1,15)%no_processors + 1)
+            address=randint(1,no_memory_lines)
+            rw=(randint(1,15)%2)
+            data=randint(1,256)                        
+            if(rw==1):            
+                fp.write(proc_id+" "+hex(address)+" "+str(rw)+" "+a)
+            else:
+                fp.write(proc_id+" "+hex(address)+" "+str(rw)+" "+hex(data)+a)
     fp.close()
 
 os.chdir(os.getcwd())
 fp=open("procID_address_rw_data.txt","w")
 x=[]
-generator(10)
+no_memory_lines=int(input(' No. of memory lines:'))
+no_processors=int(input('No. of processors:'))
+no_transactions=int(input('No. of read write transactions:'))
+
+generator(no_memory_lines,no_processors,no_transactions)
